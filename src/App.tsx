2beb5f6a1da5eab473e09f8b7e4191e5774a8f0d
Emiliano Alegre <emilianogalegre@gmail.com>
2024-02-23 18:36:22 -0300

    feature(app): Add checkbox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import "./App.css";
 
-function App() {
+interface Country {
+  id: number;
+  name: string;
+  isSelected: boolean;
+}
+
+interface CountryItemProps {
+  country: Country;
+  onToggle: (id: number) => void;
+}
+
+interface AppProps {}
+
+const initialCountries: Country[] = [
+  { id: 1, name: "India", isSelected: false },
+  { id: 2, name: "USA", isSelected: false },
+  { id: 3, name: "France", isSelected: false },
+];
+
+const CountryItem = React.memo<CountryItemProps>(({ country, onToggle }) => (
+  <li key={country.id}>
+    <label>
+      <input
+        type="checkbox"
+        checked={country.isSelected}
+        onChange={() => onToggle(country.id)}
+      />
+      {country.name}
+    </label>
+  </li>
+));
+
+const App: React.FC<AppProps> = () => {
+  const [countries, setCountries] = useState<Country[]>(initialCountries);
+  const [selectAll, setSelectAll] = useState(false);
+
+  const handleCountryToggle = useCallback((id: number) => {
+    setCountries((prevCountries) => {
+      const updatedCountries = prevCountries.map((country) =>
+        country.id === id
+          ? { ...country, isSelected: !country.isSelected }
+          : country
+      );
+      const newSelectAll = updatedCountries.every(
+        (country) => country.isSelected
+      );
+      setSelectAll(newSelectAll);
+      return updatedCountries;
+    });
+  }, []);
+
+  const handleSelectAllToggle = useCallback(() => {
+    setCountries((prevCountries) =>
+      prevCountries.map((country) => ({ ...country, isSelected: !selectAll }))
+    );
+    setSelectAll((prevSelectAll) => !prevSelectAll);
+  }, [selectAll]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>
+        <input
+          type="checkbox"
+          checked={selectAll}
+          onChange={handleSelectAllToggle}
+        />
+        Select All
+      </label>
+      <ul>
+        {countries.map((country) => (
+          <CountryItem
+            key={country.id}
+            onToggle={handleCountryToggle}
+            country={country}
+          />
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
