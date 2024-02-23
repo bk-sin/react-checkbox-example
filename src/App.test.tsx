19b5be043738319c3c4870b667e47a6359acb5fe
Emiliano Alegre <emilianogalegre@gmail.com>
2024-02-23 18:45:15 -0300

    tests: add tests for app functionalities

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  test("renders without crashing", () => {
+    render(<App />);
+  });
+
+  test("can select individual countries", () => {
+    render(<App />);
+
+    const indiaCheckbox = screen.getByLabelText("India");
+
+    fireEvent.click(indiaCheckbox);
+    expect(indiaCheckbox).toBeChecked();
+  });
+
+  test("can select and deselect all when 'Select All' is clicked", () => {
+    render(<App />);
+
+    const selectAllCheckbox = screen.getByLabelText("Select All");
+
+    fireEvent.click(selectAllCheckbox);
+    expect(selectAllCheckbox).toBeChecked();
+
+    fireEvent.click(selectAllCheckbox);
+    expect(selectAllCheckbox).not.toBeChecked();
+  });
+
+  test("deselects 'Select All' when an individual country is selected", () => {
+    render(<App />);
+
+    const selectAllCheckbox = screen.getByLabelText("Select All");
+    fireEvent.click(selectAllCheckbox);
+
+    const indiaCheckbox = screen.getByLabelText("India");
+    fireEvent.click(indiaCheckbox);
+
+    expect(selectAllCheckbox).not.toBeChecked();
+  });
+
+  test("selects 'Select All' automatically when all countries are selected", () => {
+    render(<App />);
+
+    const selectAllCheckbox = screen.getByLabelText("Select All");
+    const indiaCheckbox = screen.getByLabelText("India");
+    const usaCheckbox = screen.getByLabelText("USA");
+    const franceCheckbox = screen.getByLabelText("France");
+
+    fireEvent.click(indiaCheckbox);
+    fireEvent.click(usaCheckbox);
+    fireEvent.click(franceCheckbox);
+
+    expect(selectAllCheckbox).toBeChecked();
+  });
 });
